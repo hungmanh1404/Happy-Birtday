9519af4f9f059fedd8cd65909ef7cbbd44c7bba0
Manh Nguyen <manh.nguyen@mb137-manhnguyen.local>
2025-10-14 10:57:40 +0700

    ngọc nhi

--- assets/index-2lIHQj8D.js
+++ assets/index-2lIHQj8D.js
@@ -6433,7 +6433,7 @@ customElements.get(ji) || customElements.define(ji, tt);
           ? R.jsx("div", { id: "flame", className: "flame" })
           : R.jsx("div", { id: "flame", className: "flame out" }),
       }),
-      R.jsx("div", { className: "name", children: "20/08 - Trinh Trinh" }),
+      R.jsx("div", { className: "name", children: "08/10 - Chị Ngọc Nhị" }),
     ],
   });
 var $o = {
